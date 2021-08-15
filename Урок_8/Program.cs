f8819061db20bbfced88255e5607ee06fecc1c57
Anonim Education <soraplay111@gmail.com>
2021-08-15 12:21:54 +0300

    Добавьте файлы проекта.

---
+++ Урок_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Урок_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kort = (10, "word", 15);
+            Console.WriteLine(kort.Item1);
+            kort.Item1 = 100;
+            Console.WriteLine(kort.Item1);
+            Console.WriteLine(kort.Item2);
+            Console.WriteLine(kort.Item3);
+            Console.WriteLine();
+            var kort2 = (name: "Vladimir", age: 20);
+            Console.WriteLine(kort2.name);
+            Console.WriteLine(kort2.age);
+
+            (int, string) data = (1, "Oleksey");
+            Console.WriteLine(data.Item1);
+            Console.WriteLine(data.Item2);
+            Console.WriteLine();
+            Console.WriteLine($"ID: {data.Item1}, Name: {data.Item1}");
+        }
+    }
+}
